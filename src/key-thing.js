d48e8a2b100fe8d5ed4644442bbb5449b8438365
Tike <124289043+TarasKostr@users.noreply.github.com>
2025-11-09 19:47:38 +0200

    stuff

--- src/key-thing.js
+++ src/key-thing.js
@@ -1,5 +1,9 @@
+
+
 const keys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
 let currentKeyIndex = 0
+
+let currentKey = keys[Math.random()]
 
 window.addEventListener("keydown", (event) => {
     console.log(event.key);
